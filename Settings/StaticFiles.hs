dcb0eec1fe29f9a6f84de01f2eb98543dde6a0ba
Changaco <changaco@changaco.net>
2013-12-26 13:06:39 +0100

    minify CSS and JS

--- Settings/StaticFiles.hs
+++ Settings/StaticFiles.hs
@@ -1,17 +1,24 @@
 module Settings.StaticFiles where
 
-import Prelude (IO)
+import Prelude hiding (FilePath)
+import Data.ByteString.Lazy (ByteString)
+import Data.Default (def)
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.Encoding as TLE
+import Filesystem.Path.CurrentOS (FilePath)
+import Language.Haskell.TH (Q, Exp, Name)
+import System.Directory (doesFileExist)
+import System.Exit (ExitCode(..))
+import System.Process (callProcess, readProcessWithExitCode)
 import Yesod.Static
-import qualified Yesod.Static as Static
+
 import Settings (staticDir)
 import Settings.Development
-import Language.Haskell.TH (Q, Exp, Name)
-import Data.Default (def)
 
 -- | use this to create your static file serving site
-staticSite :: IO Static.Static
-staticSite = if development then Static.staticDevel staticDir
-                            else Static.static      staticDir
+staticSite :: IO Static
+staticSite = if development then staticDevel staticDir
+                            else static      staticDir
 
 -- | This generates easy references to files in the static directory at compile time,
 --   giving you compile-time verification that referenced files exist.
@@ -21,6 +28,21 @@ $(staticFiles Settings.staticDir)
 
 combineSettings :: CombineSettings
 combineSettings = def
+    { csCssPostProcess = minifyWith "clean-css" []
+    , csJsPostProcess = minifyWith "uglify-js" ["-mc"]
+    }
+
+minifyWith :: String -> [String] -> [FilePath] -> ByteString -> IO ByteString
+minifyWith pkg args paths input = do
+    let prog = "node_modules/"++pkg++"/bin/"++filter (/='-') pkg
+    installed <- doesFileExist prog
+    if not installed
+        then callProcess "npm" ["install", pkg]
+        else return ()
+    (code, out, err) <- readProcessWithExitCode prog args (TL.unpack $ TLE.decodeUtf8 input)
+    if code /= ExitSuccess
+        then error ("Error minifying "++show paths++": "++out++err)
+        else return (TLE.encodeUtf8 $ TL.pack out)
 
 -- The following two functions can be used to combine multiple CSS or JS files
 -- at compile time to decrease the number of http requests.
